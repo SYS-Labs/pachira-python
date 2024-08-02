744044ab3477ef97f399788911dbf8df7db11b0a
icmoore <ic3moore@gmail.com>
2024-08-02 16:05:47 -0700

    more investigation

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(
-    name="pachira-python",
+    name="pachira",
     version="0.0.1",
-    description="Python API for Pachira project",
+    description="Python library for Pachira on DeFi, data research and integration",
     long_description=long_description,
     long_description_content_type="text/markdown",    
     url="https://github.com/SYS-Labs",
     author="icmoore",
-    author_email="defipy.devs@gmail.com",
+    author_email="icmoore@syscoin.org",
     license="MIT",
     package_dir = {"pachira": "python/prod"},
     packages=[
         "pachira",
         "pachira.explorer"
     ],
-    install_requires=["requests"],
+    install_requires=["requests, blockscout-python"],
     include_package_data=True,
     zip_safe=False,
 )
